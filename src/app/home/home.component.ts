b9ddd0d733ba55ba53da245777cd27456fe25a70
jimmyhealer <yahing6066@gmail.com>
2021-07-30 22:44:41 +0800

    update the contact us zone of homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,14 @@
 // import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit, HostListener } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+export interface Reply{
+  name: string
+  phone: string
+  email: string
+}
 
 @Component({
   selector: 'app-home',
@@ -10,32 +18,39 @@ import { Component, OnInit, HostListener } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
-  contactForm: FormGroup;
-  disabledSubmitButton: boolean = true;
-  optionsSelect: Array<any> = [];
+  contactForm: FormGroup
+  disabledSubmitButton: boolean = true
+  optionsSelect: Array<any> = []
 
   @HostListener('input') oninput() {
-
-  if (this.contactForm.valid) {
-    this.disabledSubmitButton = false;
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false
     }
   }
 
-  constructor(private fb: FormBuilder) {
-
-  this.contactForm = fb.group({
-    'contactFormName': ['', Validators.required],
-    'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
-    'contactFormSubjects': ['', Validators.required],
-    'contactFormMessage': ['', Validators.required],
-    'contactFormCopy': [''],
+  constructor(private fb: FormBuilder, 
+    private readonly afs: AngularFirestore,
+    private snackBar: MatSnackBar) {
+  
+    this.contactForm = fb.group({
+      'name': ['', Validators.required],
+      'phone': ['', Validators.required],
+      'email': ['', Validators.compose([Validators.required, Validators.email])],
     });
   }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
-  
+
   onSubmit() {
+    this.contactForm.value.time = new Date()
+    this.afs.collection('reply').add(this.contactForm.value).then(()=>{
+      this.contactForm.reset()
+      this.disabledSubmitButton = true
+      this.snackBar.open('已收到，我們會盡快與您聯絡', '確認', {
+        duration: 3000
+      })
+    })
     // this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
     //   alert('Your message has been sent.');
     //   this.contactForm.reset();
