a6f4d28ae0f25bc6fd84c3a907907e6cedb71122
jimmyhealer <yahing6066@gmail.com>
2021-07-30 11:51:56 +0800

    add home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,47 @@
+// import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+  contactForm: FormGroup;
+  disabledSubmitButton: boolean = true;
+  optionsSelect: Array<any> = [];
+
+  @HostListener('input') oninput() {
+
+  if (this.contactForm.valid) {
+    this.disabledSubmitButton = false;
+    }
+  }
+
+  constructor(private fb: FormBuilder) {
+
+  this.contactForm = fb.group({
+    'contactFormName': ['', Validators.required],
+    'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+    'contactFormSubjects': ['', Validators.required],
+    'contactFormMessage': ['', Validators.required],
+    'contactFormCopy': [''],
+    });
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  onSubmit() {
+    // this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
+    //   alert('Your message has been sent.');
+    //   this.contactForm.reset();
+    //   this.disabledSubmitButton = true;
+    // }, (error: any) => {
+    //   console.log('Error', error);
+    // });
+  }
+}
