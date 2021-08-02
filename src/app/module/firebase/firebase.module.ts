df8721d633365a44d834542a980274d406174909
jimmyhealer <yahing6066@gmail.com>
2021-08-02 20:45:26 +0800

    remove commom module

--- src/app/module/firebase/firebase.module.ts
+++ src/app/module/firebase/firebase.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AngularFireModule } from '@angular/fire';
 import { firebaseConfig } from './fireconfig'
