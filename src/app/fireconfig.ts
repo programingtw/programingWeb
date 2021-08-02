35a9e5fd4c20525400dab505351b38c1fbe932b5
jimmyhealer <yahing6066@gmail.com>
2021-08-02 19:42:55 +0800

    change APP architecture, move to index module

--- src/app/fireconfig.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBapxDqfk-zEXkyOGAWJa6m7pNhmLvVsJo",
-  authDomain: "programing-972f0.firebaseapp.com",
-  projectId: "programing-972f0",
-  storageBucket: "programing-972f0.appspot.com",
-  messagingSenderId: "170863287280",
-  appId: "1:170863287280:web:433d3b910f4cea5efe1b35",
-  measurementId: "G-L1MEHJG9W0"
-};
