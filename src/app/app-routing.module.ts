4116dd685c14aea9b0a4181be5b0e31f9a0a5e51
jimmyhealer <yahing6066@gmail.com>
2021-08-02 19:41:09 +0800

    change APP architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index/index.component';
+import { AdminComponent } from './admin/admin/admin.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutusComponent},
-  {path: 'home', redirectTo: '/', pathMatch: 'full'},
-  {path: '**', redirectTo: '/', pathMatch: 'full'},
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'admin', component: AdminComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,21 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatSelectModule } from '@angular/material/select'
-import { AngularFireModule } from '@angular/fire';
+
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { CarouselComponent } from './components/carousel/carousel.component'
-
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { MatSnackBarModule } from '@angular/material/snack-bar'
-import { FooterComponent } from './components/footer/footer.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-
-import { firebaseConfig } from './fireconfig'
+import { AdminModule } from './admin/admin.module';
+import { IndexModule } from './index/index.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    CarouselComponent,
-    FooterComponent,
-    AboutusComponent
+    AppComponent
   ],
   imports: [
-    MDBBootstrapModule.forRoot(),
-    BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    MatSnackBarModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AdminModule,
+    IndexModule,
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
