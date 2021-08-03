dcbc77fa021b34aaa395d090b81eccc12795890e
jimmyhealer <yahing6066@gmail.com>
2021-08-03 14:15:42 +0800

    admin login finish

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms'
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router'
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AngularFireAuth, 
+    private router: Router,
+    private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
+  email = new FormControl('')
+  password = new FormControl('')
 
+  async login() {
+    console.log('login')
+    const credential = 
+      await this.auth.signInWithEmailAndPassword(
+        this.email.value, this.password.value
+      ).then(()=>{
+        this.snackbar.open('登入成功', '恩', {duration: 3000})
+        this.router.navigate(['/admin'])
+      }).catch(()=>{
+        this.snackbar.open('密碼錯了', '恩', {duration: 3000})
+      });
+  }
+ 
 }
