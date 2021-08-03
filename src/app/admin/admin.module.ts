0a50c9c8b4debe34c1c7aa6ee2622b0126bc14b4
jimmyhealer <yahing6066@gmail.com>
2021-08-03 14:13:40 +0800

    add component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,13 +7,17 @@ import { FirebaseAuthModule } from '../module/firebase/firebaseAuth.module';
 import { AdminComponent } from './admin/admin.component';
 import { AnnouncementComponent } from './announcement/announcement.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AnnouncementdetailComponent } from './announcement/announcementdetail/announcementdetail.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     AnnouncementComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    AnnouncementdetailComponent
   ],
   imports: [
     CommonModule,
