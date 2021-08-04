4d6fe0206dc602411c6e20c40a6056c5a106c59e
jimmyhealer <yahing6066@gmail.com>
2021-08-04 15:19:26 +0800

    update announcement detail, can upload all

--- src/app/admin/announcement/announcementdetail/announcementdetail.component.ts
+++ src/app/admin/announcement/announcementdetail/announcementdetail.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder,Validators } from '@angular/forms'
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import * as ClassicEditor from '../../../../ckeditor/ckeditor';
-import { collectExternalReferences } from '@angular/compiler';
 
 @Component({
   selector: 'app-announcementdetail',
@@ -14,16 +14,19 @@ import { collectExternalReferences } from '@angular/compiler';
 })
 export class AnnouncementdetailComponent implements OnInit {
   public editor = ClassicEditor
-  config = {
+  public config = {
     placeholder: "內容",
-    toolbar: [
-      'heading', '|',
-      'undo', 'redo', 'fontSize', 'fontColor', 'alignment', 'bold', 'italic','|',
-      'link', 'bulletedList', 'numberedList', '|',
-      'outdent', 'indent', '|',
-      'code', 'codeBlock', 'imageInsert', 'imageUpload',
-      'blockQuote', 'insertTable', 'mediaEmbed', 'sourceEditing'
-    ],
+    toolbar: {
+      items:[
+        'heading', '|',
+        'undo', 'redo', 'fontSize', 'fontColor', 'alignment', 'bold', 'italic','|',
+        'link', 'bulletedList', 'numberedList', '|',
+        'outdent', 'indent', '|',
+        'code', 'codeBlock', 'imageInsert', 'imageUpload',
+        'blockQuote', 'insertTable', 'mediaEmbed', 'sourceEditing'
+      ],
+      isSticky: true
+    },
     language: 'zh',
     image: {
       toolbar: [
@@ -41,23 +44,38 @@ export class AnnouncementdetailComponent implements OnInit {
       ]
     },
     licenseKey: '', 
-    // extraPlugins: [ this.TheUploadAdapterPlugin ]
   };
   public id: any
   public doc: Observable<any>
-  public file: File | null = null;
-
-  @ViewChild('fileInput') fileInput!: ElementRef;
-  fileAttr = 'Choose File';
+  public coverPicture: string = ''
+  public form = this.fb.group({
+    title: ['', Validators.required],
+    publishDate: ['', Validators.required],
+    publishTimePicker: ['00:00', Validators.required],
+    vis: [false],
+    intro: ['', Validators.required],
+    content: ['', Validators.required]
+  })
 
   constructor(
     private router: Router,
     private acrout: ActivatedRoute,
     private afs: AngularFirestore,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
+    private fb: FormBuilder
   ) {
     this.id = this.acrout.snapshot.paramMap.get('id')
     this.doc = this.afs.collection('announcement').doc(this.id).valueChanges()
+    this.doc.subscribe(res=>{
+      this.form.patchValue(res)
+      this.coverPicture = res.coverPicture
+      let t = new Date(res.publishTime.seconds * 1000)
+      this.form.patchValue({
+        publishDate: t,
+        publishTimePicker: String(t.getHours()) + ':' + 
+                           String(t.getMinutes()).padStart(2, '0')
+      })
+    })
   }
 
   ngOnInit(): void {
@@ -65,13 +83,48 @@ export class AnnouncementdetailComponent implements OnInit {
   }
 
   public onReady(editor) {
-    editor.ui.getEditableElement().parentElement.insertBefore(
-      editor.ui.view.toolbar.element,
-      editor.ui.getEditableElement()
-    );
     editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
       return new UploadAdapter(loader, this.storage, this.id);
     };
+  }
+
+  public imageupload(event){
+    const file = event.target.files[0];
+    const filePath = `announcement/${this.id}/${file.name}`
+    let fileref = this.storage.ref(filePath)
+    const task = this.storage.upload(filePath, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        fileref.getDownloadURL().subscribe(res => this.coverPicture = res)
+      })
+    ).subscribe()
+  }
+
+  public submit(status){
+    if(!this.form.valid){
+      alert('你還有東西沒輸入')
+      return
+    }
+    let f = this.form.value
+    let publishTime = new Date(f.publishDate)
+    let t = f.publishTimePicker.split(':')
+    publishTime.setHours(parseInt(t[0]))
+    publishTime.setMinutes(parseInt(t[1]))
+    let data = {
+      content: f.content,
+      intro: f.intro,
+      publishTime: publishTime,
+      vis: f.vis,
+      coverPicture: this.coverPicture,
+      title: f.title,
+      updateTime: new Date()
+    }
+    console.log(data, f.vis)
+    this.afs.collection('announcement').doc(this.id).update(data).then(()=>{
+      alert('success')
+    }).catch(()=>{
+      alert('壞掉了')
+    })
   }
 }
 
