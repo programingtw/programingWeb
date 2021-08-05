b84dfc0f63a0048b85ce9cecac1ac6b7734ba820
jimmyhealer <yahing6066@gmail.com>
2021-08-06 00:04:16 +0800

    add meta

--- src/app/index/announcement/announcementdetail/announcementdetail.component.ts
+++ src/app/index/announcement/announcementdetail/announcementdetail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore'
 // import { Observable } from 'rxjs';
+import { Meta } from '@angular/platform-browser'
 
 export interface Doc{
   content: string,
@@ -24,7 +25,8 @@ export class AnnouncementdetailComponent implements OnInit {
   constructor(
     private router: Router,
     private acrout: ActivatedRoute,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private meta: Meta
   ) { 
     this.id = this.acrout.snapshot.paramMap.get('id')
     this.afs.collection('announcement', 
@@ -32,10 +34,22 @@ export class AnnouncementdetailComponent implements OnInit {
       ).doc(this.id).valueChanges()
       .subscribe( val => {
         this.doc = val
+        this.meta.addTags([
+          {name: 'title', content: `普羅柯普小教室-${this.doc.title}`},
+          {name: 'description', content: this.doc.intro},
+          {name: 'author', content: `科技柯基`},
+          {name: 'canonical', content: `https://new.programing.tw/announcement/${this.id}`},
+          {name: 'og:type', content: 'article'},
+          {name: 'og:title', content: `普羅柯普小教室${this.doc.title}`},
+          {name: 'og:description', content: `${this.doc.intro}\n快來點擊看文章`},
+          {name: 'og:url', content: `https://new.programing.tw/announcement/${this.id}`},
+          {name: 'og:img', content: this.doc.coverPicture},
+          {name: 'og:site_name', content: `普羅程式教育`},
+        ])
       }, e => {
         this.router.navigate([''])
       })
-  }
+    }
 
   ngOnInit(): void {
   }
