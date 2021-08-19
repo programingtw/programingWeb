982ef730f92349c173039a3d42ce62993d61a149
jimmyhealer <yahing6066@gmail.com>
2021-08-19 23:36:09 +0800

    fixed router scroll top

--- src/app/index/index/index.component.ts
+++ src/app/index/index/index.component.ts
@@ -11,5 +11,10 @@ export class IndexComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
+  onActivate(event) {
+    window.scroll(0,0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)  
+  }
 }
